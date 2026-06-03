55cb6997a1df2c60a068371aeb8a14a2eba15807
Sheikh Abdul Wahab <sheikhabdulwahab.dev@gmail.com>
2026-06-03 23:55:56 +0500

    firebase lab task

---
+++ After_mid/React/Lab/reactproject/reactpro3/src/App.jsx
@@ -0,0 +1,53 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+
+import { db } from "./initFirebase";
+import { auth } from "./initFirebase";
+
+import { addDoc, collection , getDocs , setDoc , doc} from 'firebase/firestore'
+
+
+function App() {
+  const googleAuth = new GoogleAuthProvider()
+
+  const aunthentication = async function () {
+
+    try {
+
+      const credential = await signInWithPopup(auth, googleAuth)
+
+      alert(credential.user.displayName)
+
+      const docid = await addDoc(collection(db, "semester6user"), { name: credential.user.displayName, email: credential.user.email })
+      alert(docid.id)
+
+      await setDoc(doc(db , "sem6user1" , docid.id ) , {username: credential.user.displayName , contact: "1234"})
+
+
+
+    }
+
+    catch (error) {
+
+      alert(error)
+
+
+    }
+
+
+
+  }
+
+  return (
+
+    <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh" }} >
+
+      <button onClick={aunthentication}
+        style={{ backgroundColor: "yellow", padding: "15px", border: "2px solid black" }}> Google Aunthentication</button>
+
+    </div>
+
+  )
+
+}
+
+export default App
